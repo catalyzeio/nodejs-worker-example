1c5fd1b8bfa7fc6a429289d8b6653c2f29239e4e
Adam Leko <adam@catalyze.io>
2015-05-06 13:11:01 -0500

    Add MongoDB integration to basic example

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+    MongoClient = require('mongodb').MongoClient;
+
+var databaseURL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/demo';
 
 var app = express();
-app.get('/', function (req, res) {
-    res.send('Hello, world!');
+app.use(function (req, res, next) {
+    MongoClient.connect(databaseURL, function (err, db) {
+        if (err) {
+            next(err);
+            return;
+        }
+        // insert a new record into the visits collection
+        var collection = db.collection('visits');
+        collection.insert([{
+            message: 'Hello, world!',
+            date: new Date()
+        }], function (err, result) {
+            if (err) {
+                next(err);
+                db.close();
+                return;
+            }
+            // fetch the latest 10 visits, sorted by date
+            var cursor = collection.find().sort({'date': -1}).limit(10);
+            // accumulate these and send as JSON when done
+            var response = [];
+            cursor.each(function (err, doc) {
+                if (err) {
+                    next(err);
+                    db.close();
+                    return;
+                }
+                if (doc == null) {
+                    res.json(response);
+                    db.close();
+                    return;
+                }
+                response.push(doc);
+            });
+        });
+    });
 });
 
 var port = process.env.PORT || 8088;
