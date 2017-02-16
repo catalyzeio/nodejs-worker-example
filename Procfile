eb8ca2b0bd607e13b724dee786a5deeb7296a926
Nathan Sweet <nathanjsweet@gmail.com>
2017-02-16 13:54:14 -0600

    making app simple worker thing

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: npm start
+worker1: npm start
+worker2 npm start
--- index.js
+++ index.js
@@ -1,48 +1,10 @@
 'use strict';
 
-var express = require('express'),
-    MongoClient = require('mongodb').MongoClient;
-
-var databaseURL = process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/demo';
+var express = require('express');
 
 var app = express();
 app.get('/', function (req, res, next) {
-    MongoClient.connect(databaseURL, function (err, db) {
-        if (err) {
-            next(err);
-            return;
-        }
-        // insert a new record into the visits collection
-        var collection = db.collection('visits');
-        collection.insert([{
-            message: 'Hello, world!',
-            date: new Date()
-        }], function (err, result) {
-            if (err) {
-                next(err);
-                db.close();
-                return;
-            }
-            console.log('inserted a new record in the visits collection');
-            // fetch the latest 10 visits, sorted by date
-            var cursor = collection.find().sort({'date': -1}).limit(10);
-            // accumulate these and send as JSON when done
-            var response = [];
-            cursor.each(function (err, doc) {
-                if (err) {
-                    next(err);
-                    db.close();
-                    return;
-                }
-                if (doc == null) {
-                    res.json(response);
-                    db.close();
-                    return;
-                }
-                response.push(doc);
-            });
-        });
-    });
+    res.send('hello world!');
 });
 
 app.get('/ping', function (req, res) {
