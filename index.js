ca355243338800a980b5dd50098daa562d665db3
Nathan Sweet <nathanjsweet@gmail.com>
2017-03-06 16:54:01 -0600

    adding health check logic to example

--- index.js
+++ index.js
@@ -3,12 +3,28 @@
 var express = require('express');
 
 var app = express();
+var off = false;
 app.get('/', function (req, res, next) {
     res.send('hello world!');
 });
 
 app.get('/ping', function (req, res) {
-    res.send('pong\n');
+    if off {
+	res.writeHead(404)
+	res.close()
+    } else {
+	res.send('pong\n');
+    }
+});
+
+app.get('/off', function (req, res) {
+    off = true;
+    res.send('off\n')
+});
+
+app.get('/on', function (req, res) {
+    off = false;
+    res.send('on\n')
 });
 
 var port = process.env.PORT || 8088;
